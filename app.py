7fbddf875556ecc560c7f8efa541cc06e37054e4
Yaswanth Varma <ysagiraj@iu.edu>
2023-12-04 01:15:19 -0500

    Second

--- app.py
+++ app.py
@@ -2,10 +2,7 @@ import streamlit as st
 import pickle
 import re
 import nltk
-print(st.__version__)
-print(pickle.__version__)
-print(re.__version__)
-print(nltk.__version__)
+
 nltk.download('punkt')
 nltk.download('stopwords')
 
